76667f41dcf74331d7876ccd0aa9b622f03beb7d
piotr <piotr@regis.local>
2024-05-28 21:32:29 +0200

    Działa drugie okno

---
+++ FormDetails.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace Menadżer_Drużyn
+{
+    partial class FormDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// 
+
+        public FormDetails(string param)
+        {
+            InitializeComponent();
+        } 
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormDetails
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(730, 362);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Szczegóły";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -30,29 +30,60 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBoxKapitan1 = new System.Windows.Forms.CheckBox();
+            this.comboBoxRank1 = new System.Windows.Forms.ComboBox();
             this.textBoxName1 = new System.Windows.Forms.TextBox();
             this.pictureBoxRola1 = new System.Windows.Forms.PictureBox();
-            this.comboBoxRank1 = new System.Windows.Forms.ComboBox();
-            this.checkBoxKapitan1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonAccept = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRola1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.checkBoxKapitan1);
             this.panel1.Controls.Add(this.comboBoxRank1);
             this.panel1.Controls.Add(this.textBoxName1);
             this.panel1.Controls.Add(this.pictureBoxRola1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1257, 86);
             this.panel1.TabIndex = 0;
             // 
+            // checkBoxKapitan1
+            // 
+            this.checkBoxKapitan1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxKapitan1.AutoSize = true;
+            this.checkBoxKapitan1.Location = new System.Drawing.Point(1076, 32);
+            this.checkBoxKapitan1.Name = "checkBoxKapitan1";
+            this.checkBoxKapitan1.Size = new System.Drawing.Size(126, 29);
+            this.checkBoxKapitan1.TabIndex = 3;
+            this.checkBoxKapitan1.Text = "kapitan?";
+            this.checkBoxKapitan1.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxRank1
+            // 
+            this.comboBoxRank1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxRank1.FormattingEnabled = true;
+            this.comboBoxRank1.Items.AddRange(new object[] {
+            "unranked",
+            "bronze",
+            "silver",
+            "gold"});
+            this.comboBoxRank1.Location = new System.Drawing.Point(861, 27);
+            this.comboBoxRank1.Name = "comboBoxRank1";
+            this.comboBoxRank1.Size = new System.Drawing.Size(176, 33);
+            this.comboBoxRank1.TabIndex = 2;
+            // 
             // textBoxName1
             // 
+            this.textBoxName1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxName1.Location = new System.Drawing.Point(113, 30);
             this.textBoxName1.Name = "textBoxName1";
             this.textBoxName1.Size = new System.Drawing.Size(709, 31);
@@ -69,42 +100,44 @@
             this.pictureBoxRola1.TabIndex = 0;
             this.pictureBoxRola1.TabStop = false;
             // 
-            // comboBoxRank1
+            // groupBox1
             // 
-            this.comboBoxRank1.FormattingEnabled = true;
-            this.comboBoxRank1.Items.AddRange(new object[] {
-            "unranked",
-            "bronze",
-            "silver",
-            "gold"});
-            this.comboBoxRank1.Location = new System.Drawing.Point(861, 27);
-            this.comboBoxRank1.Name = "comboBoxRank1";
-            this.comboBoxRank1.Size = new System.Drawing.Size(176, 33);
-            this.comboBoxRank1.TabIndex = 2;
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonAccept);
+            this.groupBox1.Location = new System.Drawing.Point(12, 455);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1233, 134);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Szczegóły";
             // 
-            // checkBoxKapitan1
+            // buttonAccept
             // 
-            this.checkBoxKapitan1.AutoSize = true;
-            this.checkBoxKapitan1.Location = new System.Drawing.Point(1076, 32);
-            this.checkBoxKapitan1.Name = "checkBoxKapitan1";
-            this.checkBoxKapitan1.Size = new System.Drawing.Size(126, 29);
-            this.checkBoxKapitan1.TabIndex = 3;
-            this.checkBoxKapitan1.Text = "kapitan?";
-            this.checkBoxKapitan1.UseVisualStyleBackColor = true;
+            this.buttonAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAccept.Location = new System.Drawing.Point(1032, 46);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(178, 62);
+            this.buttonAccept.TabIndex = 0;
+            this.buttonAccept.Text = "Akceptuj";
+            this.buttonAccept.UseVisualStyleBackColor = true;
+            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1257, 601);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menadżer Drużyn";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRola1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -116,6 +149,8 @@
         private System.Windows.Forms.TextBox textBoxName1;
         private System.Windows.Forms.CheckBox checkBoxKapitan1;
         private System.Windows.Forms.ComboBox comboBoxRank1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonAccept;
     }
 }
 
