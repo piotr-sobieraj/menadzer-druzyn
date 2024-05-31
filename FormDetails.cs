3863d372673e1ca7e2e72864dcb7379ca5fe5daf
piotr <piotr@regis.local>
2024-05-31 05:57:02 +0200

    Ładują się obrazki w MainForm

--- FormDetails.cs
+++ FormDetails.cs
@@ -4,12 +4,15 @@ namespace Menadżer_Drużyn
 {
     public partial class FormDetails : Form
     {
-        public FormDetails(Player p1, Player p2)
+        public FormDetails(Player p1, Player p2, Player p3, Player p4, Player p5)
         {
             InitializeComponent();
 
             listBoxSummary.Items.Add(p1.ToString());
             listBoxSummary.Items.Add(p2.ToString());
+            listBoxSummary.Items.Add(p3.ToString());
+            listBoxSummary.Items.Add(p4.ToString());
+            listBoxSummary.Items.Add(p5.ToString());
         }
-     }
+    }
 }
